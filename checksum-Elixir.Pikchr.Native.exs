2f9012f1526d5b7d13dc5b6489809051559f49a1
Neil Berkman <neil@xuku.com>
2026-02-21 13:34:48 -0800

    Add v0.5.1 NIF checksums

---
+++ checksum-Elixir.Pikchr.Native.exs
@@ -0,0 +1,14 @@
+%{
+  "libpikchr_nif-v0.5.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:a5d24c7ebcfc6be6b95a5f2fc40332bf8240c2ec6bcdbc7f5d1835a483979fbb",
+  "libpikchr_nif-v0.5.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:c22c283b9a1d4b25f4856eb5c5d28bfd86d501018cf7ee35ed6789f3f2b14f7b",
+  "libpikchr_nif-v0.5.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:2fb2756329fe4302eba515dcc8a11a62be2538d1ee2e23e32bca48f4da6dd9c5",
+  "libpikchr_nif-v0.5.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:16c02fa424acb5cba0b5b7dbb2bdbbb7b92d87cda60392c181e292abfa116484",
+  "libpikchr_nif-v0.5.1-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:1f91fbce50f0544a12033d2b035273abbfededa425ce24db65ebfd661f4ee5df",
+  "libpikchr_nif-v0.5.1-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:1894b6ed126852300d25eb219c49a8b83438ece109803551de50c079afe6ba3e",
+  "libpikchr_nif-v0.5.1-nif-2.16-x86_64-apple-darwin.so.tar.gz" => "sha256:d7f364c4c447357b0a3720b96bdd2529086286f6a3942cbbdeda2cab0733ef32",
+  "libpikchr_nif-v0.5.1-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:0fea316acb0d875b3f16afc332f4c52ddd671991ed73a986e433affcba1f70c6",
+  "libpikchr_nif-v0.5.1-nif-2.17-aarch64-apple-darwin.so.tar.gz" => "sha256:6a81484acddb67523baacdaba1d0ecd08d403aa724eaa8dcad74c6e6a89526c2",
+  "libpikchr_nif-v0.5.1-nif-2.17-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:3bd536786b0246d7d874beaf53fb1996b92bd592ed514d870c359f02faaed1de",
+  "libpikchr_nif-v0.5.1-nif-2.17-x86_64-apple-darwin.so.tar.gz" => "sha256:daa3dcea7dce4ee3b451d89cf461bd65e37a3dadd061a753996bb1512b488e2f",
+  "libpikchr_nif-v0.5.1-nif-2.17-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:f2bc68260a83d2936b889d7f6d838421c6d7dc7e1a12c0cb7b26df5a50047b13",
+}
